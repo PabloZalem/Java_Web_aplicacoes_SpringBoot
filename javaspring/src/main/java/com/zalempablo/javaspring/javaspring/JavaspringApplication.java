444f1dd7d20e18b0b3e6f7e6e26888c1244ac8dd
PabloZalem <zalem.pablo@yahoo.com>
2023-09-14 21:06:37 -0300

    Subindo na classe a forma tbm de querer faver com .war

--- javaspring/src/main/java/com/zalempablo/javaspring/javaspring/JavaspringApplication.java
+++ javaspring/src/main/java/com/zalempablo/javaspring/javaspring/JavaspringApplication.java
@@ -2,12 +2,29 @@ package com.zalempablo.javaspring.javaspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+/*@SpringBootApplication
 public class JavaspringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaspringApplication.class, args);
 	}
+
+}
+*/
+
+@SpringBootApplication
+public class JavaspringApplication extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(JavaspringApplication.class);
+  }
+
+  public static void main(String[] args) {
+    SpringApplication.run(JavaspringApplication.class, args);
+  }
 
 }
